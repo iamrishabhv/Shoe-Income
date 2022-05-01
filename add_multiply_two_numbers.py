092f90671ac1f75751236263cbc6d0c2d26332fa
iamrishabhv <rishabhverma41@gmail.com>
2022-05-01 13:57:54 +0530

    Second Commit

---
+++ add_multiply_two_numbers.py
@@ -0,0 +1,11 @@
+#Enter Python code here and hit the Run button.
+number1 = int(input())
+number2 = int(input())
+
+product = number1 * number2
+sum = number1 + number2
+
+if (product >= 1000):
+    print(product)
+else:
+    print(sum)
